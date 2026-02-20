4d658dafb1ab03514bc2c9a67f980c50c030370b
Kartikey pandey <kartikey224252@gmail.com>
2026-02-20 11:41:19 +0530

    parser

--- PythonProject/app.py
+++ PythonProject/app.py
@@ -217,4 +217,4 @@ if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
     
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
